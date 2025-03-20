defcba7bfc03cf1c43eb16d855dac5db7f508836
olubaj <144109079+olubaj@users.noreply.github.com>
2025-03-20 14:13:04 +0100

    added income model

--- expense_tracker/core/models.py
+++ expense_tracker/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 
 # Expense Model
 class Expense(models.Model):
@@ -15,3 +15,15 @@ class Expense(models.Model):
     class Meta:
         ordering = ['-date']
 
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='incomes')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.category} - {self.amount}"
+    
+    class Meta:
+        ordering = ['-date']
