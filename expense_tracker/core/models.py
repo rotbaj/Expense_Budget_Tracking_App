c8a86699b9e34f59db8f8cf0f21a807bb7c64eb2
olubaj <144109079+olubaj@users.noreply.github.com>
2025-03-21 16:43:27 +0100

    commented out budget model, working on fix

--- expense_tracker/core/models.py
+++ expense_tracker/core/models.py
@@ -28,14 +28,14 @@ class Income(models.Model):
     class Meta:
         ordering = ['-date']
 
-# Budget Model
-class Budget(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='budgets')
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    start_date = models.DateField()
-    end_date = models.DateField()
+# # Budget Model
+# class Budget(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='budgets')
+#     amount = models.DecimalField(max_digits=10, decimal_places=2)
+#     category = models.CharField(max_length=100)
+#     description = models.TextField(blank=True, null=True)
+#     start_date = models.DateField()
+#     end_date = models.DateField()
 
-    def __str__(self):
-        return f"{self.category} - {self.amount} (From {self.start_date} to {self.end_date})"
+#     def __str__(self):
+#         return f"{self.category} - {self.amount} (From {self.start_date} to {self.end_date})"
