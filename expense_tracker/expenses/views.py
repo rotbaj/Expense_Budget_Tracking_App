57876dd52c580513d418df8e3faa26b9b99b54f6
olubaj <144109079+olubaj@users.noreply.github.com>
2025-03-26 19:17:01 +0100

    expenseviewset and categoryviewset

--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,14 +1,28 @@
-from rest_framework import viewsets
-from .models import Expense
-from .serializers import ExpenseSerializer
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
+from .models import Expense, Category
+from .serializers import ExpenseSerializer, CategorySerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ['category', 'date']
+    ordering_fields = ['amount', 'date']
+    search_fields = ['description', 'category__name']
 
     def get_queryset(self):
         return Expense.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    pagination_class = None
+
+    def perform_create(self, serializer):
+        serializer.save()
