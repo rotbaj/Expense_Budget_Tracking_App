9106ea2eb6fb76c14535052d76ce2967f3bccdf4
olubaj <144109079+olubaj@users.noreply.github.com>
2025-03-26 19:50:19 +0100

    created frontend app

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core', # Add the core app to the list of installed apps
     
     # Third-party libraries
     'rest_framework',
